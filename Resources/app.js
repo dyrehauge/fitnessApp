1446322fdaba18e7adc80e1dbe50c905ac9cf0af
Jesper Martinussen <jesperkmartinussen@gmail.com>
2014-10-28 14:41:44 +0100

    changed comments for app.js

--- Resources/app.js
+++ Resources/app.js
@@ -1,3 +1,20 @@
+/* 
+// This is the main file for Rhino Fitness App
+
+// This app is a Fitness app for iForm, which allows the users to shadowrun against themselves.
+
+// v. 1.0
+
+// Folders:
+ 
+	ui/  -------------  All the pages goes here
+	functions/ -------  All the functions goes here
+	img/ -------------- All the images goes here
+	misc/ ------------- Everything else goes here (fonts eg.)
+	
+*/
+
+// initialize the welcome screen
 (function() {
 	var app = require('ui/welcome-view')();
 })();
